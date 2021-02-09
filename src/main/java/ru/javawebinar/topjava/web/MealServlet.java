4b5dccf03d7487e132de20287e0ca3f6818d3088
Александр Мягков <a.myagkov@itlc.ru>
2021-02-09 20:06:13 +0300

    compete HW1

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,12 +1,17 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.TimeUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -17,7 +22,12 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        List<MealTo> meals = MealsUtil.getMeals().stream()
+                .map(meal -> MealsUtil.createTo(meal, meal.getCalories() > 500))
+                .collect(Collectors.toList());
+
+        request.setAttribute("meals", meals);
+        request.setAttribute("formatter", TimeUtil.formatter);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,11 +1,42 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" %>
+<jsp:useBean id="meals" scope="request" type="java.util.List<ru.javawebinar.topjava.model.MealTo>"/>
+<jsp:useBean id="formatter" scope="request" type="java.time.format.DateTimeFormatter"/>
+
 <html lang="ru">
 <head>
     <title>Meals</title>
+    <style type="text/css">
+        table {
+            border-collapse: collapse;
+        }
+
+        td, th {
+            padding: 4px;
+            border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<table>
+    <tbody>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+
+    <c:forEach var="meal" items="${meals}">
+        <tr style="color: ${meal.excess ? "red" : "green"}">
+            <td><c:out value="${meal.dateTime.format(formatter)}"/></td>
+            <td><c:out value="${meal.description}"/></td>
+            <td><c:out value="${meal.calories}"/></td>
+        </tr>
+    </c:forEach>
+    </tbody>
+</table>
 </body>
 </html>
